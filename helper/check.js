47ec0ec965b9a817f4739662cb2d4930b4775b21
jiqinghua <jiqinghua@souche.com>
2016-04-02 00:47:38 +0800

    feature: use check.js to filter unnormal data

---
+++ helper/check.js
@@ -0,0 +1,43 @@
+const songci = require('../data/songci.json');
+const fs = require('fs');
+
+const checkedSognci = [];
+const unCheckedSognci = [];
+
+/**
+ * 检查是否有不标准的字符
+ */
+function checkContent(text) {
+	if (/[A-Za-z0-9□\\＿/]/.test(text) === false) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * 循环剔除脏数据
+ */
+songci.forEach(function (item) {
+	if (checkContent(item.content) && checkContent(item.title) && checkContent(item.author)) {
+		checkedSognci.push(item);
+	} else {
+		unCheckedSognci.push(item);
+	}
+});
+
+/**
+ * 将新的数据写入 checked.json
+ */
+const checkedFileFullPath = '../data/checked.json';
+const unCheckedFileFullPath = '../data/unchecked.json';
+fs.writeFileSync(checkedFileFullPath, JSON.stringify(checkedSognci), 'utf8', (err) => {
+	if (err) {
+		console.error(err);
+	}
+});
+fs.writeFileSync(unCheckedFileFullPath, JSON.stringify(unCheckedSognci), 'utf8', (err) => {
+	if (err) {
+		console.error(err);
+	}
+});
